f2c8fc6394a1e55368f4dd8883f8acec8c4b20c0
Kunal <bkunal455@gmail.com>
2021-03-29 08:19:41 +0530

    personal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { NgxSkillBarModule } from "ngx-skill-bar";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSkillBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
